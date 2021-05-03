386eca1d0720293eb43ad7b57e37a088d080c7fd
Minh Tran <minhtran.se@gmail.com>
2021-05-03 20:12:24 +0700

    Successfully setup project with MaterialUI styles in Class Component and MuiThemeProvider as well.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { 
+  CssBaseline, Container, Grid, Typography, 
+  Card, CardActions, CardHeader, CardContent
+} from '@material-ui/core';
 
-export default App;
+import { withStyles, MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+// import {  } from './components';
+
+//USAGE MATERIAL-UI STYLES IN CLASS COMPONENT
+const useStyles = myStyles => ({
+  card: {
+    background: 'linear-gradient(55deg, hsla(212, 35%, 58%, 1) 0%, hsla(218, 32%, 80%, 1) 100%)',
+
+  },
+});
+
+//USE MATERIAL-UI THEME FOR APP BACKGROUND
+const theme = createMuiTheme({
+  overrides: {
+    MuiCssBaseline: {
+      "@global": {
+        body: {
+          backgroundColor: '#0d1117', //copied from Github.com
+          color: '',
+        },
+      },
+    },
+  },
+});
+
+//DEFINE THE MAIN CLASS COMPONENT
+class App extends React.Component {
+
+  //DEFINE states to be used in the App
+  // state = {}
+
+  render() {
+
+    //USAGE MATERIAL-UI STYLES IN CLASS COMPONENT
+    const { classes } = this.props;
+
+    return (
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+
+        <Container>
+          <Grid container justify="center">
+            <Grid item xs={12} sm={6}>
+              <Card className={classes.card}>
+                <CardHeader title="Some header text goes here" />
+                <CardContent>
+                  <Typography variant="body2" color="textSecondary">
+                  This impressive paella is a perfect party dish and a fun meal to cook together with your
+          guests. Add 1 cup of frozen peas along with the mussels, if you like.
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          </Grid>
+        </Container>
+
+      </MuiThemeProvider>
+    ) //END return()
+
+  } //END render()
+
+}// END CLASS COMPONENT
+
+// export default App;
+    
+//USAGE MATERIAL-UI STYLES IN CLASS COMPONENT
+export default withStyles(useStyles)(App);
